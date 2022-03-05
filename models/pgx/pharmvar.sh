b36ec9690913bee2ccf8d4523533a1d3fcb906ad
marpiech <piechota.marcin@gmail.com>
2022-03-05 21:22:14 +0000

    pahrmavr script

--- models/pgx/pharmvar.sh
+++ models/pgx/pharmvar.sh
@@ -2,24 +2,44 @@
 
 # pharmvar.sh - a script to download pharmvar database
 
+# working directory
+export WORKING_DIR=/tmp
+
 # url of the pharmvar database
-blob:https://www.pharmvar.org/f7190663-b4cb-46ce-b943-134e91132877
-url="ftp://ftp.ebi.ac.uk/pub/databases/pharmgkb/pharmvar/latest/pharmvar.tsv.gz"
-url="ftp://ftp.ebi.ac.uk/pub/databases/pharmgkb/pharmgkb_latest.tar.gz"
+# curl 'https://www.pharmvar.org/get-download-file?name=ALL&refSeq=ALL&fileType=zip&version=current' \
+#   -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0' \
+#   -H 'Accept: */*' \
+#   -H 'Accept-Language: en-US,en;q=0.5' \
+#   -H 'Accept-Encoding: gzip, deflate, br' \
+#   -H 'Connection: keep-alive' \
+#   -H 'Referer: https://www.pharmvar.org/download' \
+#   -H 'Sec-Fetch-Dest: empty' \
+#   -H 'Sec-Fetch-Mode: cors' \
+#   -H 'Sec-Fetch-Site: same-origin' \
+#   -H 'DNT: 1' \
+#   -H 'Sec-GPC: 1' \
+#   --output $WORKING_DIR/pharmvar.zip
+
+# unzip $WORKING_DIR/pharmvar.zip -d /tmp/
+
+export VERSION=$(ls $WORKING_DIR | grep "pharmvar-" | sed 's/.*-//' | sort -r | head -n 1)
+
+function parse_gene {
+    local GENE=$1
+    local GENE_LOWERCASE=$(echo $GENE | tr '[:upper:]' '[:lower:]')
+    local MODEL_FILE="$GENE_LOWERCASE-pharmvar-$VERSION.yml"
+    echo "haplotype_model:" > $MODEL_FILE
+    echo "  variants:" >> $MODEL_FILE
+    echo "    $GENE*1.001:" >> $MODEL_FILE
+    
+}
+export -f parse_gene
+
+ls /tmp/pharmvar-$VERSION/ | xargs parse_gene 
 
 
-
-# array of genes to download
-export GENES=(
-
-
-"CYP2A13", "CYP2B6", "CYP2C8", "CYP2C9", "CYP2C19", "CYP2D6", "CYP2E1", "CYP2F1", "CYP2J2", "CYP2R1", "CYP2S1", "CYP2W1", "CYP3A4", "CYP3A5", "CYP3A7", "CYP3A43", "CYP4F2", "SLCO1B1", "DPYD", "NUDT15")
-
-echo "haplotype_model:" > cyp2d6-pharmvar.yml
-echo "  variants:" >> cyp2d6-pharmvar.yml
-echo "    CYP2D6*1.001:" >> cyp2d6-pharmvar.yml
-ls *.vcf | \
-  sort -k1,1V | \
-  xargs -i bash -c \
-    'HAPLOTYPE=$(echo {} | sed "s|.vcf||" | sed "s|_|\*|"); echo "    $HAPLOTYPE:"; cat {} | grep -v "#" | sed "s|chr||"' | \
-  awk '{if ($1 ~ /^22/) {chr=$1; pos=$2; ref=$4; alt=$5; id=chr"-"pos"-"ref"-"alt; print "      "id": {ref: \""ref"\", alt: \""alt"\", effect_allele: \""alt"\"}"} else {print $0}}' >> cyp2d6-pharmvar.yml
+# ls *.vcf | \
+#   sort -k1,1V | \
+#   xargs -i bash -c \
+#     'HAPLOTYPE=$(echo {} | sed "s|.vcf||" | sed "s|_|\*|"); echo "    $HAPLOTYPE:"; cat {} | grep -v "#" | sed "s|chr||"' | \
+#   awk '{if ($1 ~ /^22/) {chr=$1; pos=$2; ref=$4; alt=$5; id=chr"-"pos"-"ref"-"alt; print "      "id": {ref: \""ref"\", alt: \""alt"\", effect_allele: \""alt"\"}"} else {print $0}}' >> cyp2d6-pharmvar.yml
