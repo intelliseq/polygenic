0da8bc0fc77b9ea6c230711577ed9bcb77d91dfe
marpiech <piechota.marcin@gmail.com>
2022-03-14 13:22:48 +0100

    Moving development from remote to local

--- sh/cyp2d6.sh
+++ sh/cyp2d6.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-echo "haplotype_model:" > cyp2d6-pharmvar.yml
-echo "  variants:" >> cyp2d6-pharmvar.yml
-echo "    CYP2D6*1.001:" >> cyp2d6-pharmvar.yml
-ls *.vcf | \
-  sort -k1,1V | \
-  xargs -i bash -c \
-    'HAPLOTYPE=$(echo {} | sed "s|.vcf||" | sed "s|_|\*|"); echo "    $HAPLOTYPE:"; cat {} | grep -v "#" | sed "s|chr||"' | \
-  awk '{if ($1 ~ /^22/) {chr=$1; pos=$2; ref=$4; alt=$5; id=chr"-"pos"-"ref"-"alt; print "      "id": {ref: \""ref"\", alt: \""alt"\", effect_allele: \""alt"\"}"} else {print $0}}' >> cyp2d6-pharmvar.yml
+export VERSION=$(ls pharmvar-* | head -1 | grep -oP '[0-9]+\.[0-9]+\.[0-9]+')
+mkdir -p models
+
+function generate_model {
+  GENE=$1
+  GENE_LOWERCASE=$(echo "$GENE" | tr '[:upper:]' '[:lower:]')
+  echo $GENE
+  echo $GENE_LOWERCASE
+  echo "haplotype_model:" > models/$GENE_LOWERCASE-pharmvar.yml
+  echo "  variants:" >> models/$GENE_LOWERCASE-pharmvar.yml
+  echo "    $GENE*1.001:" >> models/$GENE_LOWERCASE-pharmvar.yml
+  ls pharmvar-$VERSION/$GENE/GRCh38/*.vcf | \
+    sort -k1,1V | \
+    xargs -i bash -c \
+      'HAPLOTYPE=$(echo {} | sed "s|'pharmvar-$VERSION/$GENE/GRCh38/'||" | sed "s|.vcf||" | sed "s|_|\*|"); echo "    $HAPLOTYPE:"; cat {} | grep -v "#" | sed "s|chr||"' | \
+  awk '{if ($1 ~ /^22/) {chr=$1; pos=$2; ref=$4; alt=$5; id=chr"-"pos"-"ref"-"alt; print "      "id": {ref: \""ref"\", alt: \""alt"\", effect_allele: \""alt"\"}"} else {print $0}}' >> models/$GENE_LOWERCASE-pharmvar.yml
+}
+export -f generate_model
+
+ls pharmvar-$VERSION | xargs -i bash -c 'generate_model {}'
